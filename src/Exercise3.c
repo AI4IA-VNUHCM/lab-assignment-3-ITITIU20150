b2d787a17bee5f3ffc13e9b294e17b7521810843
ITITIU20150 <91869941+ITITIU20150@users.noreply.github.com>
2021-10-18 17:42:14 +0700

    ITITIU20150

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -14,8 +14,60 @@ Ex:
 
 void Ex3(int in_arr[], int n){
 	//Your codes here
-	
+	int max=0;
+	int count = 0;
+	int clone_arr[1000];
+	int arr_result[1000];
+	for (int i = 0, j = i + 1; i < n - 1 && j < n; i++, j++)
+	{
+		if (in_arr[i] <= in_arr[j])
+		{
+			clone_arr[count] = in_arr[i];
+			count += 1;
+		}
+		if (in_arr[i] > in_arr[j])
+		{
+			clone_arr[count] = in_arr[i];
+			if (max < count)
+			{
+				max = count;
+				for (int k = 0; k <= count; k++)
+					arr_result[k] = clone_arr[k];
+			}
+			count = 0;
+		}
+	}
+	printf("Increasing ");
+	for (int i = 0; i <= max; i++)
+		printf("%d ", arr_result[i]);
+	printf("Decreasing ");
+	count = 0;
+	max = 0;
+	for (int i = 0, j = i + 1; i < n - 1 && j < n; i++, j++)
+	{
+		if (in_arr[i] >= in_arr[j])
+		{
+			clone_arr[count] = in_arr[i];
+			count += 1;
+		}
+		if (in_arr[i] < in_arr[j])
+		{
+			clone_arr[count] = in_arr[i];
+			if (max < count)
+			{
+				max = count;
+				for (int k = 0; k <= count; k++)
+					arr_result[k] = clone_arr[k];
+			}
+			count = 0;
+		}
+	}
+	for (int i = 0; i <= max; i++)
+		printf("%d ", arr_result[i]);
 }
+
+
+
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
